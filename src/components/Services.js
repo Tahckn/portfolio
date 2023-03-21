e1299cc62404f78d46d658186a31daa36312416e
Taha Ceken <taha-ceken@hotmail.com>
2023-03-21 16:40:52 +0300

    added links to services

--- src/components/Services.js
+++ src/components/Services.js
@@ -5,22 +5,25 @@ import { motion } from 'framer-motion'
 
 const services = [
   {
-    name: 'Development',
+    name: 'Web Development',
     description:
       'With the help of various JavaScript frameworks, including React Vue Next and Nuxt, I can create single-page or multi-page web applications. Additionally, include a cloud database and implement real-time data.',
-    link: 'Learn more',
+    linkText: 'Learn more',
+    link: 'https://en.wikipedia.org/wiki/Web_development',
   },
   {
     name: 'Product Branding',
     description:
       'By developing an effective brand strategy, companies are able to establish long-term relationships with their target audiences and create loyalty among consumers who identify with their brands.',
-    link: 'Learn more',
+    linkText: 'Learn more',
+    link: 'https://en.wikipedia.org/wiki/Brand',
   },
   {
     name: 'Deploying',
     description:
-      'By developing an effective brand strategy, companies are able to establish long-term relationships with their target audiences and create loyalty among consumers who identify with their brands.',
-    link: 'Learn more',
+      'I can deploy websites on a web server so they can be accessed by anyone with an internet connection. This process involves writing code and creating visuals, testing the website, then publishing it to a web server. The development process is important because it ensures that the website will work properly and load quickly in any browser, as well as being optimized for mobile devices.',
+    linkText: 'Learn more',
+    link: 'https://simple.wikipedia.org/wiki/Web_hosting_service',
   },
 ]
 
@@ -64,7 +67,7 @@ const Services = () => {
             {/* service list  */}
             <div>
               {services.map((service, index) => {
-                const { name, description, link } = service
+                const { name, description, linkText, link } = service
                 return (
                   <div
                     className="border-b border-white/20 h-auto mb-[38px] flex text-center lg:text-left flex-col lg:flex-row"
@@ -80,13 +83,20 @@ const Services = () => {
                     </div>
                     <div className="mt-4 flex flex-col whitespace-nowrap flex-1 items-center lg:items-end lg:mt-0 justify-center lg:justify-start ">
                       <a
-                        href="#"
+                        target="_blank"
+                        href={link}
+                        rel="noreferrer"
                         className="btn w-9 h-9 flex items-center justify-center mb-2"
                       >
                         <BsArrowUpRight />
                       </a>
-                      <a href="#" className="text-gradient text-xs">
-                        {link}
+                      <a
+                        href={link}
+                        rel="noreferrer"
+                        target="_blank"
+                        className="text-gradient text-xs"
+                      >
+                        {linkText}
                       </a>
                     </div>
                   </div>
